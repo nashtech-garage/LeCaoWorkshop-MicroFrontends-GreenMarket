103d9b5d53f04f52ebad5ff2dcb2247d8a535d0d
HARVEYNASH\duc.nguyenthanh <duc.nguyenthanh@nashtechglobal.com>
2023-04-24 10:10:26 +0700

    init the project

---
+++ react-apps/common-hero-footer/src/root.component.tsx
@@ -0,0 +1,9 @@
+import HeroFooter from "./components/hero-footer/hero-footer.component";
+
+export default function Root(props) {
+    return (
+        <div className="row">
+            <div className="col-lg-9"><HeroFooter/></div>
+        </div>
+    );
+}
