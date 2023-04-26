f233e49da2df08e85f389826b33630d14da89ef4
HARVEYNASH\duc.nguyenthanh <duc.nguyenthanh@nashtechglobal.com>
2023-04-26 16:01:48 +0700

    update code

--- react-apps/common-footer-app/src/components/hero-footer/hero-footer.component.tsx
+++ react-apps/common-footer-app/src/components/hero-footer/hero-footer.component.tsx
@@ -3,79 +3,84 @@ import styles from "../../root.module.css";
 
 export default function HeroFooter() {
     return (
-        <footer className="footer spad" id="hero-footer-container">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-3 col-md-6 col-sm-6">
-                <div className="footer__about">
-                  <div className="footer__about__logo">
-                    <a href="./index.html"><img src="img/logo.png" alt="" /></a>
-                  </div>
-                  <ul>
-                    <li>Address: VietinBank Tran Quoc Toan, Da Nang</li>
-                    <li>Phone: 082-654-0246</li>
-                    <li>Email: duc.nguyenthanh@nashtechglobal.com</li>
-                  </ul>
-                </div>
+      <footer className={styles["footer"]}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className={styles["footer__about"]}>
+              <div className={styles["footer__about__logo"]}>
+                <a href="./index.html"
+                  ><img src="assets/img/logo.png" alt=""
+                /></a>
               </div>
-    
-              <div className="col-lg-4 offset-lg-1 col-md-6 col-sm-6">
-                <div className="footer__widget">
-                  <h6>Useful Links</h6>
-                  <ul>
-                    <li><a href="#">About Us</a></li>
-                    <li><a href="#">About Our Shop</a></li>
-                    <li><a href="#">Secure Shopping</a></li>
-                    <li><a href="#">Delivery infomation</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                    <li><a href="#">Our Sitemap</a></li>
-                  </ul>
-                  <ul>
-                    <li><a href="#">Who Are We</a></li>
-                    <li><a href="#">Our Services</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Contact</a></li>
-                    <li><a href="#">Innovation</a></li>
-                    <li><a href="#">Testimonials</a></li>
-                  </ul>
-                </div>
-              </div>
-    
-              <div className="col-lg-4 col-md-12">
-                <div className="footer__widget">
-                  <h6>Join Our Newsletter Now</h6>
-                  <p>Get E-mail updates about our latest shop and special offers.</p>
-                  <form action="#">
-                    <input type="text" placeholder="Enter your mail" />
-                    <button type="submit" className="site-btn">Subscribe</button>
-                  </form>
-                  <div className="footer__widget__social">
-                    <a href="#"><i className="fa fa-facebook" /></a>
-                    <a href="#"><i className="fa fa-instagram" /></a>
-                    <a href="#"><i className="fa fa-twitter" /></a>
-                    <a href="#"><i className="fa fa-pinterest" /></a>
-                  </div>
-                </div>
-              </div>
+              <ul>
+                <li>Address: Da Nang</li>
+                <li>Phone: +84 826540246</li>
+                <li>Email: duc.nguyenthanh@nashtechglobal.com</li>
+              </ul>
             </div>
-    
-            <div className="row">
-              <div className="col-lg-12">
-                <div className="footer__copyright">
-                  <div className="footer__copyright__text">
-                    <p>
-                      Copyright &copy;
-                      <script>document.write(new Date().getFullYear());</script>
-                      All rights reserved | <b>TK</b>
-                    </p>
-                  </div>
-                  <div className="footer__copyright__payment">
-                    <img src="img/payment-item.png" alt="" />
-                  </div>
-                </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
+            <div className={styles["footer__widget"]}>
+              <h6>Useful Links</h6>
+              <ul>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">About Our Shop</a></li>
+                <li><a href="#">Secure Shopping</a></li>
+                <li><a href="#">Delivery infomation</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Our Sitemap</a></li>
+              </ul>
+              <ul>
+                <li><a href="#">Who We Are</a></li>
+                <li><a href="#">Our Services</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Innovation</a></li>
+                <li><a href="#">Testimonials</a></li>
+              </ul>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-12">
+            <div className={styles["footer__widget"]}>
+              <h6>Join Our Newsletter Now</h6>
+              <p>
+                Get E-mail updates about our latest shop and special offers.
+              </p>
+              <form action="#">
+                <input type="text" placeholder="Enter your mail" />
+                <button type="submit" className="site-btn">Subscribe</button>
+              </form>
+              <div className={styles["footer__widget__social"]}>
+                <a href="#"><i className="fa fa-facebook"></i></a>
+                <a href="#"><i className="fa fa-instagram"></i></a>
+                <a href="#"><i className="fa fa-twitter"></i></a>
+                <a href="#"><i className="fa fa-pinterest"></i></a>
               </div>
             </div>
           </div>
-        </footer>
+        </div>
+        <div className="row">
+          <div className="col-lg-12">
+            <div className={styles["footer__copyright"]}>
+              <div className={styles["footer__copyright__text"]}>
+                <p>
+                  Copyright &copy;
+                  <script>
+                    document.write(new Date().getFullYear());
+                  </script>
+                  All rights reserved | This template is made with
+                  <i className="fa fa-heart" aria-hidden="true"></i> by
+                  <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                </p>
+              </div>
+              <div className={styles["footer__copyright__payment"]}>
+                <img src="assets/img/payment-item.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
       );
 }
--- react-apps/common-footer-app/src/root.component.tsx
+++ react-apps/common-footer-app/src/root.component.tsx
@@ -1,8 +1,8 @@
 import HeroFooter from "./components/hero-footer/hero-footer.component";
-
+import styles from "./root.module.css";
 export default function Root(props) {
     return (
-        <div className="row">
+        <div className={styles["footer"]}>
             <div className="col-lg-12"><HeroFooter/></div>
         </div>
     );
