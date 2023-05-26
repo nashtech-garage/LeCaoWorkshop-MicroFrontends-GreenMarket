7816e886e3cf659eb910037926db4e4e859c083e
Huynh Le <toifixbugdao@gmail.com>
2023-05-26 21:59:52 +0700

    add review content

--- react-apps/react-shop-page-product-detail-app/src/components/product-detail/ProductDetail.component.tsx
+++ react-apps/react-shop-page-product-detail-app/src/components/product-detail/ProductDetail.component.tsx
@@ -142,6 +142,7 @@ export default function ProductDetail() {
                                     id="tabs-3" role="tabpanel">
                                     <div className="product__details__tab__desc">
                                         <h6>Reviews ({shopDetailInfo.review_count})</h6>
+                                        <p>Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Pellentesque in ipsum id orci porta dapibus. Proin eget tortor risus. Vivamus suscipit tortor eget felis porttitor volutpat. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec velit neque, auctor sit amet aliquam vel, ullamcorper sit amet ligula. Proin eget tortor risus.</p>
                                     </div>
                                 </div>
                             </div>
