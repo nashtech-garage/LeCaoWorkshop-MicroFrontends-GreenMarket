c8656486850b9fd9d949e33673582a39fe10c982
Huynh Le <huynhlex@nashtechglobal.com>
2023-06-21 17:15:54 +0700

    Using Identity Server to create login api. The login api should be login with username & password, then return the access token.

---
+++ Identity Server/AuthServer/AuthServer/Config.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace AuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile(),
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("resourceapi", "Resource API")
+                {
+                    Scopes = {new Scope("api.read")}
+                }
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new[]
+            {
+                new Client {
+                    RequireConsent = false,
+                    ClientId = "microFE",
+                    ClientName = "Micro Frontent",
+                    ClientSecrets = {new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
+                    AllowedScopes = {"api.read"},
+                    RedirectUris = {"http://localhost:9000/auth-callback"},
+                    PostLogoutRedirectUris = {"http://localhost:9000/"},
+                    AllowedCorsOrigins = {"http://localhost:9000"},
+                    AllowAccessTokensViaBrowser = true,
+                    AccessTokenLifetime = 3600
+                }
+            };
+        }
+    }
+}
