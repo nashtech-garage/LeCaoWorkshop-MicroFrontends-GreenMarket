5f87ac56e61dda251744d756835eac152a10d59a
Nguyễn Công Danh <ncdanh19@gmail.com>
2023-07-18 23:51:19 +0700

    Create vue mainpage banner

---
+++ vue-apps/mainpage-banner-app/src/components/BannerComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="banner">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-6 col-md-6 col-sm-6">
+              <div class="banner__pic">
+                <img :src="imgHostUrl('img/banner/banner-1.jpg')" alt="banner1" />
+              </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+              <div class="banner__pic">
+                <img :src="imgHostUrl('img/banner/banner-2.jpg')" alt="banner2" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+</template>
+  
+  <script>
+  export default {
+    name: 'BannerComponent',
+    data() {
+    },
+  computed: {
+    imgHostUrl(path) {
+      return process.env.VUE_APP_COMMON_URL + path;
+    },
+  },
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  </style>
+  
