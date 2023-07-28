b4c2a97a905264ea1e03fc80df66bfed5f115e98
NgonNguyen <ntngon7@gmail.com>
2023-07-28 22:07:13 +0700

    merge code from develop branch (v0.0.30) and resolve conflict
    merge code from develop branch (v0.0.30) and resolve conflict

--- vue-apps/mainpage-banner-app/src/components/BannerComponent.vue
+++ vue-apps/mainpage-banner-app/src/components/BannerComponent.vue
@@ -1,36 +1,33 @@
 <template>
-    <div class="banner">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-6 col-md-6 col-sm-6">
-              <div class="banner__pic">
-                <img :src="imgHostUrl('img/banner/banner-1.jpg')" alt="banner1" />
-              </div>
-            </div>
-            <div class="col-lg-6 col-md-6 col-sm-6">
-              <div class="banner__pic">
-                <img :src="imgHostUrl('img/banner/banner-2.jpg')" alt="banner2" />
-              </div>
-            </div>
+  <div class="banner">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div class="banner__pic">
+            <img :src="imgHostUrl + '/img/banner/banner-1.jpg'" alt="banner1" />
+          </div>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+          <div class="banner__pic">
+            <img :src="imgHostUrl + '/img/banner/banner-2.jpg'" alt="banner2" />
           </div>
         </div>
       </div>
+    </div>
+  </div>
 </template>
-  
-  <script>
-  export default {
-    name: 'BannerComponent',
-    data() {
-    },
+
+<script>
+export default {
+  name: "BannerComponent",
+  data() {},
   computed: {
-    imgHostUrl(path) {
-      return process.env.VUE_APP_COMMON_URL + path;
+    imgHostUrl() {
+      return process.env.VUE_APP_IMAGE_SERVER_URL;
     },
   },
-  }
-  </script>
-  
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-  <style scoped>
-  </style>
-  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
